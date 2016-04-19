1513112602f916f218ab603d366a03b1429dfde2
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-04-19 16:17:49 +0900

    StartUp

---
+++ TGS01/Assets/Suzuki/Sprict/ActorParam.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Actorの情報
+/// </summary>
+public class ActorParam {
+
+	//public PolygonCollider2D mCollider;
+
+	public int mObjectID;
+
+}
---
+++ TGS01/Assets/Suzuki/Sprict/DebugGimmick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugGimmick : Actor {
+
+	private void Start()
+	{
+		base.Init();
+	}
+
+	public override void Init()
+	{
+
+	}
+}
